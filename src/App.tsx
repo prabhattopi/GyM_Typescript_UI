0291496a636310885b9eb7b6551646cd37c00224
prabhattopi <topi9864@gmail.com>
2023-01-07 21:24:55 +0530

    the file is updated with project-setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import Navbar from "@/scences/navbar";
+function App() {
+
+
+  return (
+    <div className="app bg-gray-20">
+
+      <Navbar/>
+    
+    </div>
+  )
+}
+
+
+
+export default App
