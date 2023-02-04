7f86996016f68065f3f41ed42c7785ef77381d1e
prabhattopi <topi9864@gmail.com>
2023-02-04 16:37:43 +0530

    The benefits page

--- src/scences/benefits/index.tsx
+++ src/scences/benefits/index.tsx
@@ -1,7 +1,32 @@
 import HText from "@/shared/HText";
-import { SelectedPage } from "@/shared/types";
+import { BenefitType, SelectedPage } from "@/shared/types";
 import { HomeModernIcon,UserGroupIcon,AcademicCapIcon } from "@heroicons/react/24/solid"
 import { motion } from "framer-motion";
+import Benefit from "./Benefit";
+
+const benefits:Array<BenefitType>=[
+  {
+    icon:<HomeModernIcon className="h-6 w-6"/>,
+    title: "State of the Art Facilities",
+    description: "ljof fidjijf djfofdj fjf jdfjdf jdfdf",
+
+
+  },
+  {
+    icon:<UserGroupIcon className="h-6 w-6"/>,
+    title: "100's of Diverse Classes",
+    description: "ljof fidjijf djfofdj fjf jdfjdf jdfdf",
+
+
+  },
+  {
+    icon:<AcademicCapIcon className="h-6 w-6"/>,
+    title: "Expert and Pro Trainers",
+    description: "ljof fidjijf djfofdj fjf jdfjdf jdfdf",
+
+
+  },
+]
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
@@ -15,11 +40,26 @@ const Benefits = ({setSelectedPage}: Props) => {
      <motion.div
       onViewportEnter={()=>setSelectedPage(SelectedPage.Benefits)}
      >
-      <div>
+      <div className="md:my-5 md:w-3/5">
        <HText>MORE THAN JUST A GYM.</HText>
-       
-      </div>
+       <p className="my-5 text-sm">
+        We provide world class fitness equipments, trainers and classes get you to your fitness goals with ease. We provide training care into each and every member.
 
+       </p>
+      </div>
+      {/* BENEFITS */}
+      <div className="md:flex items-center justify-between gap-8 mt-5">
+        {benefits.map((benefit:BenefitType)=>(
+          <Benefit
+          key={benefit.title}
+          icon={benefit.icon}
+          title={benefit.title}
+          description={benefit.description}
+          setSelectedPage={setSelectedPage}
+
+          />
+        ))}
+      </div>
      </motion.div>
 
     </section>
